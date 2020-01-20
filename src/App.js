e8977653e0358dbecd02b610454c14aaaa9eae93
nolan <nolan796@live.ca>
2020-01-19 19:24:55 -0700

    Add todo tasks, add input field that counts remaining tasks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import  React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 
 // Adding Dynamic Data
@@ -12,24 +12,62 @@ export default class App extends Component
     this.state =
     {
       userName : "Nolan",
-      course : "React Zero to Hero"
+      todoItems : [{ action : "Buy a car", done: false },
+                   { action : "Do Workout", done: true },
+                   { action : "Study Programming", done: false },
+                   { action : "Call a friend", done: true }],
+      newItemText : " "
     }
   }
 
+  updateNewTextValue = ( event ) => 
+   {
+      this.setState({ newItemText : event.target.value })
+
+   }
+
+   createNewTodo = () => 
+   {
+
+    if (!this.state.todoItems
+      .find(item => item.action === this.state.newItemText))
+      {
+        this.setState({
+          todoItems : [...this.state.todoItems, 
+                        { action : this.state.newItemText,
+                        done: false }],
+                        newItemText: ""
+        });
+      }
+   }
+
+
+
   changeStateData = () => {
     this.setState (
-      {userName: this.state.userName == "Nolan" ? "Riley" : "Nolan" }
+      {userName: this.state.userName === "Nolan" ? "Riley" : "Nolan" }
     )
   };
 
   render = () => 
     <div>
       <h4 className="bg-primary text-white text-center p-2">
-        { this.state.userName } Course
-        <button className="btn btn-danger m-2"
-          onClick={ this.changeStateData }>
-          Change The State  
-        </button>
+        { this.state.userName }'s Todo List
+        ({ this.state.todoItems.filter(t=> !t.done).length}) items todo
+          
       </h4>
+      <div className="container-fluid">
+        <div className="m-1">
+          <input className="form-control"
+            value={this.state.newItemText}
+            onChange={this.updateNewTextValue}
+            />
+            <button className="btn btn-danger mt-1"
+              onClick={this.createNewTodo}>
+                Add a New Task
+              </button>
+            
+        </div>
+      </div>
     </div>
 }
